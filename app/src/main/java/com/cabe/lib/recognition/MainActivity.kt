489394ec3041b08ceddece52071ed77ecd086980
沈建芳 <shenjianfang@u51.com>
2020-11-13 18:51:39 +0800

    [MDF]首次导入

---
+++ app/src/main/java/com/cabe/lib/recognition/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.cabe.lib.recognition
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.fastjson.JSONObject
+import com.aliyun.aliyunface.api.ZIMFacade
+import com.aliyun.aliyunface.api.ZIMFacadeBuilder
+
+const val TAG = "AliRecognition"
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+    fun actionInit(view: View) {
+        ZIMFacade.install(this)
+        Log.w(TAG, "sdk init")
+    }
+
+    fun actionMeta(view: View) {
+        val result = ZIMFacade.getMetaInfos(this)
+        Log.w(TAG, "meta: $result")
+    }
+
+    fun actionVerify(view: View) {
+        ZIMFacadeBuilder.create(this).verify("", true) {
+            Log.w(TAG, "verify: ${JSONObject.toJSONString(it)}")
+            true
+        }
+    }
+}
